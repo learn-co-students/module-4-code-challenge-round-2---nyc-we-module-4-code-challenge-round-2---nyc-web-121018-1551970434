423fc93681b1c6bf2eaf05f6b53aee1cb7034198
sharicodes <srosenb@yahoo.com>
2019-03-07 11:04:03 -0500

    almost

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,32 +1,65 @@
-import React, { Component } from 'react'
-import TransactionsList from './TransactionsList'
-import Search from './Search'
-import {transactions} from '../transactionsData'
+import React, { Component } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+import { transactions } from "../transactionsData";
 
 class AccountContainer extends Component {
-
   constructor() {
-    super()
+    super();
+    this.state = {
+      transactions: [],
+      searchInput: "",
+      filteredTransactions: []
+    };
 
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
-
   }
 
-  handleChange(event) {
+  handleChange = event => {
     // your code here
+    console.log(event.target.value);
+    this.setState({
+      searchInput: event.target.value
+    });
+  };
+
+  filteredTransactions = () => {
+    return this.state.transactions.filter(
+      transaction =>
+        transaction.description
+          .toLowerCase()
+          .includes(this.state.searchInput.toLowerCase()) ||
+        transaction.category
+          .toLowerCase()
+          .includes(this.state.searchInput.toLowerCase())
+    );
+  };
+
+  componentDidMount() {
+    fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+      .then(response => response.json())
+      .then(transactions =>
+        //console.log(transactions);
+        this.setState({
+          transactions: transactions
+        })
+      );
   }
 
   render() {
-
+    //console.log(this.state.transactions);
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        <TransactionsList
+          transactions={this.state.transactions}
+          filteredTransaction={this.filteredTransactions}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default AccountContainer
+export default AccountContainer;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Search = () => {
+const Search = props => {
+  //console.log(props.handleChange);
   return (
     <div className="ui huge fluid icon input">
       <input
         type="text"
         placeholder={"Search your Recent Transactions"}
+        onChange={props.handleChange}
       />
-      <i className="circular search link icon"></i>
+      <i className="circular search link icon" />
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
